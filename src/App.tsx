df52478e3815ad10b9f9ecc9b4c22aebecb78d4a
ana0 <sarah.anne.friend@gmail.com>
2025-10-22 21:57:23 +0200

    Switch video

--- src/App.tsx
+++ src/App.tsx
@@ -133,7 +133,7 @@ function App() {
         loop
         playsInline
       >
-        <source src="/video.mp4" type="video/mp4" />
+        <source src="https://pub-166474990ea24709a41c8e491c22ddfe.r2.dev/output_montage.mp4" type="video/mp4" />
         Your browser does not support the video tag.
       </video>
       {isMuted && (
